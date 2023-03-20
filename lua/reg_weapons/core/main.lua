d0a69cc170061b734d4ce1b2dd60206887667c21
Reaghan Taylor <reg_58@yahoo.co.uk>
2023-03-20 22:07:42 +0000

    feat: add ability to put teams in tables :)

--- lua/reg_weapons/core/main.lua
+++
@@ -1,5 +0,0 @@
-hook.Add("PlayerSpawn", "RegimentalWeapons", function(ply)
-    if (not RegimentalWeapons.Config.ChoiceJobs[ply:Team()]) then return end
-
-    RegimentalWeapons.Network:openMenu(ply)
-end)
